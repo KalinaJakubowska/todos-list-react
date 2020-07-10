b6480b81112a20c500678c51a5a3899991491ac3
Kalina Jakubowska <witillie@gmail.com>
2020-07-10 17:22:06 +0200

    just formatting

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -5,14 +5,14 @@ const Buttons = ({ tasks, isHidingEnabled }) => (
 
     <div className="buttonContainer">
         {tasks.length > 0 && (
-            <React.Fragment>
+            <>
                 <button className="buttonContainer__button">
                     {isHidingEnabled ? "Pokaż" : "Ukryj"} ukończone
                 </button>
                 <button className="buttonContainer__button" disabled={tasks.every(({ done }) => done)}>
                     Ukończ wszystkie
                 </button>
-            </React.Fragment>
+            </>
         )}
     </div>
 );
