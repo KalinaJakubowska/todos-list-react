495fb8b3be56ff489be02f6fca42a3d5da48b381
Kalina Jakubowska <witillie@gmail.com>
2020-07-18 09:17:37 +0200

    hide done tasks interaction added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from "./Form";
 import List from "./List";
 import Buttons from "./Buttons";
@@ -11,23 +11,30 @@ const tasks = [
   { id: 2, name: "Powtórzyć punkt pierwszy", done: false },
 ];
 
-const isHidingEnabled = false;
-
 function App() {
-  return (
-      <Container>
-        <Header title="Lista zadań" />
-        <Section
-          title="Dodaj nowe zadanie"
-          body={<Form />}
-        />
+  const [isHidingEnabled, setIsHidingEnabled] = useState(false);
+  const toggleHideDone = () => {
+    setIsHidingEnabled(isHidingEnabled => !isHidingEnabled);
+  }
 
-        <Section
-          title="Lista zadań"
-          extraHeaderContent={<Buttons tasks={tasks} isHidingEnabled={isHidingEnabled} />}
-          body={<List tasks={tasks} isHidingEnabled={isHidingEnabled} />}
-        />
-      </Container>
+  return (
+    <Container>
+      <Header title="Lista zadań" />
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form />}
+      />
+      <Section
+        title="Lista zadań"
+        extraHeaderContent=
+        {<Buttons
+          tasks={tasks}
+          isHidingEnabled={isHidingEnabled}
+          toggleHideDone={toggleHideDone}
+        />}
+        body={<List tasks={tasks} isHidingEnabled={isHidingEnabled} />}
+      />
+    </Container>
   );
 }
 export default App;
--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./style.css";
 
-const Buttons = ({ tasks, isHidingEnabled }) => (
-
+const Buttons = ({ tasks, isHidingEnabled, toggleHideDone }) => (
     <div className="buttons">
         {tasks.length > 0 && (
             <>
-                <button className="buttons__button">
+                <button
+                    className="buttons__button"
+                    onClick={toggleHideDone}
+                >
                     {isHidingEnabled ? "Pokaż" : "Ukryj"} ukończone
                 </button>
                 <button className="buttons__button" disabled={tasks.every(({ done }) => done)}>
