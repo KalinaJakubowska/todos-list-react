dc28182b527a11638c39270e7fb626fa96b92c74
Kalina Jakubowska <witillie@gmail.com>
2020-07-21 21:08:27 +0200

    empty task list text added

--- src/List/index.js
+++ src/List/index.js
@@ -1,30 +1,36 @@
 import React from "react";
 import "./style.css";
 
-const List = ({ tasks, isHidingEnabled, deleteTask, toggleTaskDone }) => (
-    <ul className="list">
-        {tasks.map(({ id, name, done }) => (
-            <li
-                key={id}
-                className={`list__item${done && isHidingEnabled ? " list__item--hidden" : ""}`}
-            >
-                <button
-                    onClick={() => toggleTaskDone(id)}
-                    className="list__button"
+const List = ({ tasks, isHidingEnabled, deleteTask, toggleTaskDone }) => {
+    if (!tasks.length) {
+        return <p>Aktualnie nie masz żadnych zadań do wykonania. Ciesz się wolnym czasem :)</p>;
+    }
+    
+    return (
+        <ul className="list">
+            {tasks.map(({ id, name, done }) => (
+                <li
+                    key={id}
+                    className={`list__item${done && isHidingEnabled ? " list__item--hidden" : ""}`}
                 >
-                    {done ? "✔" : ""}
-                </button>
-                <span className={`list__taskName ${done ? "list__taskName--done" : ""}`}>
-                    {name}
-                </span>
-                <button
-                    onClick={() => deleteTask(id)}
-                    className="list__button list__button--delete"
-                >
-                    🗑
-                </button>
-            </li>
-        ))}
-    </ul>
-)
+                    <button
+                        onClick={() => toggleTaskDone(id)}
+                        className="list__button"
+                    >
+                        {done ? "✔" : ""}
+                    </button>
+                    <span className={`list__taskName ${done ? "list__taskName--done" : ""}`}>
+                        {name}
+                    </span>
+                    <button
+                        onClick={() => deleteTask(id)}
+                        className="list__button list__button--delete"
+                    >
+                        🗑
+                    </button>
+                </li>
+            ))}
+        </ul>
+    )
+}
 export default List;
