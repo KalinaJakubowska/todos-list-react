fb6ccd191c42709c8ab9913bd36a57b72d8bebd6
Kalina Jakubowska <witillie@gmail.com>
2020-09-02 16:57:05 +0200

    theme created

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+    color: {
+        silver: "#ccc",
+        gallery: "#eee",
+        white: "#fff",
+        green: "green",
+        red: "red",
+        teal: "teal",
+        transparent: "transparent",
+    },
+    breakpoint: {
+        mobileMax: 767,
+    },
+}
