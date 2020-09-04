b643b2071eb3db4cbb1b45d0c5b160dbc0b28458
Kalina Jakubowska <witillie@gmail.com>
2020-09-04 04:11:32 +0200

    darkTheme created

--- src/theme.js
+++ src/theme.js
@@ -13,3 +13,19 @@ export const theme = {
         mobileMax: 767,
     },
 }
+
+export const darkTheme = {
+    color: {
+        main: "#8DB9B9",
+        second: "#3C4453",
+        shadow: "black",
+        background: "#272A30",
+        transparent: "transparent",
+        check: "green",
+        remove: "red",
+        text: "#EBEDEF",
+    },
+    breakpoint: {
+        mobileMax: 767,
+    },
+}
