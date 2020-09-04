108232d49f9ef41d20a2375ae007cf383be70fff
Kalina Jakubowska <witillie@gmail.com>
2020-09-04 05:25:21 +0200

    changing button text added

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { toggleTheme } from "../../themeSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleTheme, selectTheme } from "../../themeSlice";
 import { Wrapper, Button } from "./styled";
 
 const Header = ({ title }) => {
+    const isDarkTheme = useSelector(selectTheme);
     const dispatch = useDispatch();
     return (
         <Wrapper>
             <h1>{title}</h1>
-            <Button onClick={() => dispatch(toggleTheme())}>Zmień styl</Button>
+            <Button onClick={() => dispatch(toggleTheme())}>
+                {isDarkTheme ? "Jasny motyw" : "Ciemny motyw"}
+            </Button>
         </Wrapper>
     )
 }
