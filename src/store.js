4bc88340b3ad2a22a1658399cfb5d5200389b313
Kalina Jakubowska <witillie@gmail.com>
2020-09-04 04:13:02 +0200

    darkTheme created

--- src/store.js
+++ src/store.js
@@ -1,14 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import tasksReducer from "./features/tasks/tasksSlice";
+import themeReducer from "./themeSlice";
 
 const store = configureStore({
     reducer: {
         tasks: tasksReducer,
+        theme: themeReducer,
     },
 });
 
 store.subscribe(() => {
     localStorage.setItem("tasks", JSON.stringify(store.getState().tasks.tasks));
+    localStorage.setItem("theme", JSON.stringify(store.getState().theme.darkTheme));
 });
 
 export default store;
