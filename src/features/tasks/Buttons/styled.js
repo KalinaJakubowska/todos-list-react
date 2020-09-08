dd3c138754e37082a347429976a269e2e9ab4002
Kalina Jakubowska <witillie@gmail.com>
2020-09-08 09:10:15 +0200

    align-items: center added to ButtonsContainer

--- src/features/tasks/Buttons/styled.js
+++ src/features/tasks/Buttons/styled.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const ButtonsContainer = styled.div`
     display: flex;
+    align-items: center;
 
     @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
         flex-direction: column;
