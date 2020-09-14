e45d5aea343894a599ac1bb9d6f8afe0fd8f8311
Kalina Jakubowska <witillie@gmail.com>
2020-09-14 23:56:12 +0200

    Input width changed

--- src/features/tasks/Input/index.js
+++ src/features/tasks/Input/index.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export default styled.input`
+    width: 100%;
     height: 55px;
     border: 3px solid ${({ theme }) => theme.color.background};
     padding: 10px;
