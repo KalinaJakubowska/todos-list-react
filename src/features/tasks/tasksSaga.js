a24bee1aa8153cee3a85d0910e7262cb04f9dc56
Kalina Jakubowska <witillie@gmail.com>
2020-09-14 23:12:30 +0200

    bug fixed

--- src/features/tasks/tasksSaga.js
+++ src/features/tasks/tasksSaga.js
@@ -17,7 +17,7 @@ function* fetchExampleTasksHandler() {
 };
 
 function* saveTasksInLocalStorageHandler() {
-    const { tasks } = yield select(selectTasks);
+    const tasks = yield select(selectTasks);
     yield call(saveTasksInLocalStorage, tasks);
 }
 
