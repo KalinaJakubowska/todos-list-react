818971586235a65a39552dee0a96d9e46f26ad36
Kalina Jakubowska <witillie@gmail.com>
2020-09-15 09:52:13 +0200

    @media padding added to Header

--- src/common/Header/styled.js
+++ src/common/Header/styled.js
@@ -5,12 +5,15 @@ export const Wrapper = styled.header`
     justify-content: space-between;
     align-items: center;
 
+    @media(max-width: 1100px) {
+        padding: 0 10px;
+    }
+
     @media(max-width: ${({ theme }) => theme.breakpoint.mini}px ) {
         flex-direction: column;
         margin-bottom: 20px;
     }
-`
-
+`;
 export const Button = styled.button`
     border: none;
     background-color: ${({ theme }) => theme.color.sectionBackground};
@@ -20,4 +23,4 @@ export const Button = styled.button`
     display: block;
     padding: 10px;
     cursor: pointer;
-`
+`;
