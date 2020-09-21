a66d8761da092d07eb4bad316c0199b9b2237f9d
Kalina Jakubowska <witillie@gmail.com>
2020-09-22 00:22:36 +0200

    new folder created for Button

--- src/features/tasks/TasksPage/Buttons/index.js
+++ src/features/tasks/TasksPage/Buttons/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { ButtonsContainer} from "./styled";
-import { Button } from "./../styled";
+import { Button } from "../../Button";
 import {
     selectTasks,
     selectIsHidingEnabled,
